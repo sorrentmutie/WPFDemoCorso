86203ff23167938ee07c79559801554467f75203
salvatore <salvatore.sorrentino@live.com>
2023-03-10 15:02:25 +0100

    Aggiunta interfacce

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoCorso.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,31 @@ namespace DemoCorso
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GestioneOrdini? gestioneOrdine = null;
         public MainWindow()
         {
             InitializeComponent();
+            IGestioneNotifiche gestioneNotifiche = null;
+            gestioneOrdine = new GestioneOrdini(gestioneNotifiche);
+        }
+
+        private void InviaOrdine(object sender, RoutedEventArgs e)
+        {
+            
+            gestioneOrdine!.CreaOrdine(new Ordine
+            {
+                Id = 1,
+                Data = DateTime.Today,
+                ImportoTotale = 1000.0M,
+                Cliente = new Cliente
+                {
+                    Id = 23,
+                    RagioneSociale = "Mario Rossi",
+                    Indirizzo = "Via del Pino 1"
+                }
+            });
+            var totaleOrdine = gestioneOrdine.NumeroOrdiniCreati;
+            ordiniTotali.Content = totaleOrdine;
         }
     }
 }
